58bdaae54140a1c500a9c01db4e17b6aa4ceab72
cis101 <cis101@secs.iub.edu.bd>
2019-06-10 09:10:49 +0600

    Task 2a

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ console.log(heading.innerText);
 //heading.innerText = "Yo What up?"
 /* Task 2 using plain JavaScript */
 
-
+//Task 2a: Click on button to add a 'p' element
+var textBtn = document.getElementById("add text")
+textBtn.addEventListener('click', function(event){
+    var task2A = document.getElementById("task2a")
+    var paragraph = document.createElement('p')
+    paragraph.innerText = "Hello World"
+    task2A.appendChild(paragraph)
+})
 
 /* Task 4 using jQuery */
